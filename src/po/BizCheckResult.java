d8774959eeee719db7c9ffc3c40396e2a4959f04
bowieu <fatalitywolf@foxmail.com>
2017-09-12 15:13:35 +0800

    添加逻辑

---
+++ src/po/BizCheckResult.java
@@ -0,0 +1,107 @@
+package po;
+
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+/**
+ * BizCheckResult entity. @author MyEclipse Persistence Tools
+ */
+@Entity
+@Table(name = "BIZ_CHECK_RESULT",schema="BCUBBO3")
+public class BizCheckResult implements java.io.Serializable {
+
+	// Fields
+
+	private Long id;
+	private BizClaimVoucher bizClaimVoucher;
+	private Date checkTime;
+	private String checkerSn;
+	private String result;
+	private String comm;
+
+	// Constructors
+
+	/** default constructor */
+	public BizCheckResult() {
+	}
+
+	/** full constructor */
+	public BizCheckResult(Long id, BizClaimVoucher bizClaimVoucher,
+			Date checkTime, String checkerSn, String result, String comm) {
+		this.id = id;
+		this.bizClaimVoucher = bizClaimVoucher;
+		this.checkTime = checkTime;
+		this.checkerSn = checkerSn;
+		this.result = result;
+		this.comm = comm;
+	}
+
+	// Property accessors
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "chk_result")
+	@SequenceGenerator(name = "chk_result", sequenceName = "SEQ_CHECK_RESULT", allocationSize = 1)
+	@Column(name = "ID", unique = true, nullable = false, precision = 10, scale = 0)
+	public Long getId() {
+		return this.id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "CLAIM_ID")
+	public BizClaimVoucher getBizClaimVoucher() {
+		return this.bizClaimVoucher;
+	}
+
+	public void setBizClaimVoucher(BizClaimVoucher bizClaimVoucher) {
+		this.bizClaimVoucher = bizClaimVoucher;
+	}
+
+	@Column(name = "CHECK_TIME", length = 7)
+	public Date getCheckTime() {
+		return this.checkTime;
+	}
+
+	public void setCheckTime(Date checkTime) {
+		this.checkTime = checkTime;
+	}
+
+	@Column(name = "CHECKER_SN", length = 50)
+	public String getCheckerSn() {
+		return this.checkerSn;
+	}
+
+	public void setCheckerSn(String checkerSn) {
+		this.checkerSn = checkerSn;
+	}
+
+	@Column(name = "RESULT", length = 50)
+	public String getResult() {
+		return this.result;
+	}
+
+	public void setResult(String result) {
+		this.result = result;
+	}
+
+	@Column(name = "COMM", length = 500)
+	public String getComm() {
+		return this.comm;
+	}
+
+	public void setComm(String comm) {
+		this.comm = comm;
+	}
+
+}
---
+++ src/po/SysPosition.java
@@ -0,0 +1,84 @@
+package po;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+/**
+ * SysPosition entity. @author MyEclipse Persistence Tools
+ */
+@Entity
+@Table(name = "SYS_POSITION",schema="BCUBBO3")
+public class SysPosition implements java.io.Serializable {
+
+	// Fields
+
+	private Long id;
+	private String nameCn;
+	private String nameEn;
+	private Set<SysEmployee> sysEmployees = new HashSet<SysEmployee>(0);
+
+	// Constructors
+
+	/** default constructor */
+	public SysPosition() {
+	}
+
+	/** full constructor */
+	public SysPosition(Long id, String nameCn, String nameEn) {
+		this.id = id;
+		this.nameCn = nameCn;
+		this.nameEn = nameEn;
+	}
+
+	// Property accessors
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "pos")
+	@SequenceGenerator(name = "pos", sequenceName = "HIBERNATE_SEQUENCE", allocationSize = 1)
+	@Column(name = "ID", unique = true, nullable = false, precision = 10, scale = 0)
+	public Long getId() {
+		return this.id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	@Column(name = "NAME_CN", length = 50)
+	public String getNameCn() {
+		return this.nameCn;
+	}
+
+	public void setNameCn(String nameCn) {
+		this.nameCn = nameCn;
+	}
+
+	@Column(name = "NAME_EN", length = 50)
+	public String getNameEn() {
+		return this.nameEn;
+	}
+
+	public void setNameEn(String nameEn) {
+		this.nameEn = nameEn;
+	}
+	
+	@OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, mappedBy = "sysPosition")
+	public Set<SysEmployee> getSysEmployees() {
+		return this.sysEmployees;
+	}
+
+	public void setSysEmployees(Set<SysEmployee> sysEmployees) {
+		this.sysEmployees = sysEmployees;
+	}
+
+}
