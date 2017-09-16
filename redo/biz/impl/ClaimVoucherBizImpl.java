afff58110dda2b479b08ef02c44fcb69aa3728c0
bowieu <fatalitywolf@foxmail.com>
2017-09-16 15:45:56 +0800

    实现分页查询功能

--- redo/biz/impl/ClaimVoucherBizImpl.java
+++ redo/biz/impl/ClaimVoucherBizImpl.java
@@ -1,8 +1,10 @@
 package biz.impl;
 
 import java.util.Date;
+import java.util.List;
 
 import org.hibernate.criterion.DetachedCriteria;
+import org.hibernate.criterion.Property;
 import org.hibernate.criterion.Restrictions;
 
 import biz.ClaimVoucherBiz;
@@ -49,12 +51,33 @@ public class ClaimVoucherBizImpl implements ClaimVoucherBiz {
 		
 		
 		//1，查询符合条件的记录总数
+		criteria.setProjection(Property.forName("id").count());
+		//投影查询，使用一个单独数据防止全遍历，并按照给定的条件查询
+		int totalCount = claimVoucherDao.getTotalCount(criteria);
+		result.setTotalCount(totalCount);
+		if(totalCount != 0 ) {
+			
+			int totalPage = (totalCount+result.getPageSize()-1)/result.getPageSize();
+			result.setTotalPageCount(totalPage);
+			if(result.getCurrPageNo()>totalPage) {
+				result.setCurrPageNo(totalPage);
+				//有默认值，将默认值覆盖的方法，默认值为1所以当页面总数为0时设置为0
+			}
+			//2，分页查询
+			criteria.setProjection(null);
+			//将投影查询的条件进行清空处理
+			List<ClaimVoucher> items = claimVoucherDao.findForPage(criteria, result.getCurrPageNo(), result.getPageSize());
+			//传参查询获得列表文件
+			//整体都是以公共的PaginationSupport对象进行数据的封装和传递
+			result.setItems(items);
+			//将最后的数据进行封装，还要进行非空的判断
+		}
 		
 		
-		//2，分页查询
 		
 		
-		return null;
+		
+		return result;
 	}
 
 	public ClaimVoucherDao getClaimVoucherDao() {
